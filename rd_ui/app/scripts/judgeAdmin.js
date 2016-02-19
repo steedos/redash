95081da72ded34d29a4ae181dc8e3f6eb018bbd4
chenzhipei <chenzhipei@chenzhipei.local>
2016-02-19 15:17:49 +0800

    页面登录js修改

--- rd_ui/app/scripts/judgeAdmin.js
+++ rd_ui/app/scripts/judgeAdmin.js
@@ -2,7 +2,7 @@ function checkUser(){
 	if('admin'!=$("#inputEmail").val())
 	{
 		alert(1)
-		$("#login").attr("action", "/non_admin");
+		$("#login").attr("action", "/login?next=%non_admin%2");
 		// return false
 	}
 	return true;
